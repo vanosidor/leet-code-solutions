922e652ec179fb39f85e374c0d590959426302a0
vanosidor <vanosidor@gmail.com>
2021-04-07 17:01:11 +0300

    make 4Sum solution (using hash map but slow - maybe O(n^3))

--- src/four_sum/Solution.java
+++ src/four_sum/Solution.java
@@ -1,17 +1,40 @@
 package four_sum;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        if (nums == null || nums.length < 4) return res;
+        if (nums == null || nums.length < 4) return new ArrayList<>();
+        Set<List<Integer>> res = new HashSet<>();
 
         Arrays.sort(nums);
+        Map<Integer, List<int[]>> map = new HashMap<>();
         int n = nums.length;
 
-//      TODO make solution (move from left and right)
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int sum = nums[i] + nums[j];
 
-        return null;
+                if (map.containsKey(target - sum)) {
+                    List<int[]> indexesList = map.get(target - sum);
+                    for (int[] indexes : indexesList) {
+                        if (indexes[1] < i) {
+                            List<Integer> candidate = Arrays.asList(nums[indexes[0]], nums[indexes[1]], nums[i], nums[j]);
+                            res.add(candidate);
+                        }
+                    }
+                }
+                List<int[]> value = map.getOrDefault(sum, new ArrayList<>());
+                value.add(new int[]{i, j});
+                map.put(sum, value);
+            }
+        }
+        return new ArrayList<>(res);
     }
 }
